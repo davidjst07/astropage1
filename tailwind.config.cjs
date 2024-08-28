3919299833ce15255ee82c9b4ef524d42739ceaa
davidjst07 <davidjst@hotmail.com>
2024-08-28 18:12:57 -0500

    Second Commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+import { plugin } from "postcss";
+
+module.exports = {
+    content: ['./src/**/*/.{astro,html,js,jsx}'],
+    theme: {
+        extend: {
+
+        }
+    },
+    plugins:[require('tailwindcss/line-clamp')]
+}
